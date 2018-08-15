ab3961cc3ba571751d0835496f26fe5cd314a346
unknown <murchaldo@gmail.com>
2018-08-15 05:37:54 -0600

    Completing github updates

--- browserapi/ex_30/script.js
+++ browserapi/ex_30/script.js
@@ -1,6 +1,7 @@
 function removeAll(elementName) {
     listItems = document.querySelector(elementName);
     while (listItems.lastChild) {
+        console.log(listItems.lastChild);
         listItems.removeChild(listItems.lastChild)
     }
 }
